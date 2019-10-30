ceb2c2a425c4d1c91f933a413f666a24e6939c6f
has.well <dmmatrix222@gmail.com>
2019-10-30 17:23:32 +0200

    added validation tests fixed Lhun algorithm https://github.com/cloudipsp/php-sdk-v2/issues/2

--- lib/Helper/ValidationHelper.php
+++ lib/Helper/ValidationHelper.php
@@ -2,6 +2,9 @@
 
 namespace Cloudipsp\Helper;
 
+use DateTime;
+use InvalidArgumentException;
+
 class ValidationHelper
 {
     /**
@@ -18,10 +21,10 @@ class ValidationHelper
             if (is_array($param))
                 self::validateRequiredParams($params[$key], $param);
             if (!array_key_exists($key, $params)) {
-                throw new \InvalidArgumentException(sprintf('Required param "%s" is missing', $key));
+                throw new InvalidArgumentException(sprintf('Required param "%s" is missing', $key));
             }
             if (array_key_exists($key, $required) && empty($param)) {
-                throw new \InvalidArgumentException(sprintf('Required param "%s" is empty', $key));
+                throw new InvalidArgumentException(sprintf('Required param "%s" is empty', $key));
             }
             switch ($param) {
                 case 'integer':
@@ -49,12 +52,12 @@ class ValidationHelper
     /**
      * Helper method for validating URLs
      * @param      $url
-     * @throws \InvalidArgumentException
+     * @throws InvalidArgumentException
      */
     public static function validateURL($url)
     {
         if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-            throw new \InvalidArgumentException(sprintf("%s is not a fully qualified URL", $url));
+            throw new InvalidArgumentException(sprintf("%s is not a fully qualified URL", $url));
         }
     }
 
@@ -67,11 +70,11 @@ class ValidationHelper
      */
     public static function validateDate($date, $key, $format)
     {
-        $d = \DateTime::createFromFormat($format, $date);
+        $d = DateTime::createFromFormat($format, $date);
         if ($d && $d->format($format) == $date) {
             return true;
         } else {
-            throw new \InvalidArgumentException(sprintf('Date %s is incorrect! Accepted format is: %s', $key, $format));
+            throw new InvalidArgumentException(sprintf('Date %s is incorrect! Accepted format is: %s', $key, $format));
         }
     }
 
@@ -82,7 +85,7 @@ class ValidationHelper
     public static function validateInteger($param, $key = '')
     {
         if (trim($param) != null && !is_numeric($param)) {
-            throw new \InvalidArgumentException(sprintf('%s is not a valid numeric', $key));
+            throw new InvalidArgumentException(sprintf('%s is not a valid numeric', $key));
         }
     }
 
@@ -93,10 +96,10 @@ class ValidationHelper
     public static function validateString($param, $key = '')
     {
         if ($key !== 'order_id' && $param != null && !is_string($param)) {
-            throw new \InvalidArgumentException(sprintf('%s is not a valid string', $key));
+            throw new InvalidArgumentException(sprintf('%s is not a valid string', $key));
         }
         if ($key === 'order_id' && $param == null) {
-            throw new \InvalidArgumentException(sprintf('%s is not a valid string', $key));
+            throw new InvalidArgumentException(sprintf('%s is not a valid string', $key));
         }
     }
 
@@ -109,11 +112,11 @@ class ValidationHelper
     {
         $checksum = 0;
         for ($i = (2 - (strlen($number) % 2)); $i <= strlen($number); $i += 2) {
-            $checksum += (int)($number{$i - 1});
+            $checksum += (int)($number[$i - 1]);
         }
         // Analyze odd digits in even length strings or even digits in odd length strings.
         for ($i = (strlen($number) % 2) + 1; $i < strlen($number); $i += 2) {
-            $digit = (int)($number{$i - 1}) * 2;
+            $digit = (int)($number[$i - 1]) * 2;
             if ($digit < 10) {
                 $checksum += $digit;
             } else {
@@ -123,7 +126,7 @@ class ValidationHelper
         if (($checksum % 10) == 0) {
             return true;
         } else {
-            throw new \InvalidArgumentException(sprintf('\'%s\' is not a valid credit card number', $number));
+            throw new InvalidArgumentException(sprintf('\'%s\' is not a valid credit card number', $number));
         }
     }
 
@@ -136,7 +139,7 @@ class ValidationHelper
         if (filter_var($ip, FILTER_VALIDATE_IP)) {
             return true;
         } else {
-            throw new \InvalidArgumentException(sprintf('\'%s\' is not a valid ip', $ip));
+            throw new InvalidArgumentException(sprintf('\'%s\' is not a valid ip', $ip));
         }
     }
 }
